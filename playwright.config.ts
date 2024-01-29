50662ee2b87cd703a51db70825f89179bbeb8572
egormuratov <egor_muratov@hotmail.com>
2024-01-29 17:32:42 +0700

    add allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
-
+import { testPlanFilter } from "allure-playwright/dist/testplan";
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -9,6 +9,8 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+
 export default defineConfig({
   testDir: './e2e-tests',
   /* Run tests in files in parallel */
@@ -20,7 +22,17 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+    reporter: [
+      ["line"],
+      [
+        "allure-playwright",
+        {
+          detail: true,
+          outputFolder: "allure-results",
+          suiteTitle: false,
+        },
+      ],
+    ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
