2281e61dbbf2eeb3e4aabc5dc6c96e6a92b2344f
egormuratov <egor_muratov@hotmail.com>
2024-02-27 11:17:01 +0700

    add weekends service link

--- constants/explore/DirectionUrl.ts
+++ constants/explore/DirectionUrl.ts
@@ -1,3 +1,4 @@
 export const MOSCOW_NP_FLIGHT = '/?params=MOWNP1'
 export const MOSCOW_LONDON_WITH_DATES_21JUNE_17JULY = '/?params=MOW2106LON17071'
 export const MULTIWAY_PARAMS_MOW_IST_5JULY_IST_LON_9OCTOBER = '/?params=MOW0507IST0910LON1'
+export const WEEKENDS_SERVICE = '?params=MOW1&service=weekends'
